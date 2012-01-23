5534955d560ad609917e12a4b6fd9172f712e060
Ben Chambers <bchambers@clearfoundation.com>
2012-01-23 20:12:01 +0000

    - Updated description

--- language/en_US/print_server_lang.php
+++ language/en_US/print_server_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang['print_server_app_name'] = 'Advanced Print Server';
-$lang['print_server_app_description'] = 'Print Server description...'; // FIXME
+$lang['print_server_app_description'] = 'The Print Server app provides a common print interface to printers on the network.  This allows clients on the LAN to share print resources and eases administration into one centralized service.';
 $lang['print_server_management_tool_not_accessible'] = 'The Print Server management tool is not available when the server is not running';
 $lang['print_server_management_tool'] = 'Print Server Management Tool';
 $lang['print_server_management_tool_help'] = 'Follow the link to access the Print Server management tool.';
