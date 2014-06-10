6ab1d690a6d414d7d64ecabef107e947937ad0db
clearostranslations <translations@clearfoundation.com>
2014-06-09 22:39:46 -0400

    Updated translation

--- language/en_US/print_server_lang.php
+++ language/en_US/print_server_lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['print_server_app_name'] = 'Advanced Print Server';
 $lang['print_server_app_description'] = 'The Print Server app provides a common print interface to printers on the network.  This allows clients on the LAN to share print resources and eases administration into one centralized service.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Go to Print Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Follow the link to access the Print Server management tool.';
 $lang['print_server_management_tool_not_accessible'] = 'The Print Server management tool is not available when the server is not running';
 $lang['print_server_management_tool'] = 'Print Server Management Tool';
-$lang['print_server_management_tool_help'] = 'Follow the link to access the Print Server management tool.';
-$lang['print_server_go_to_management_tool'] = 'Go to Print Server Management Tool';
