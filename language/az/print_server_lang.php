07ce4c39496261315da833e14b9ac940cf2cb96e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:11:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Advanced Print Server app şəbəkə printerlər üçün ortaq çap interfeysi təmin edir. Bu LAN müştərilərə çap resursları bölüşmək üçün imkan verir və bir mərkəzləşdirilmiş xidmətə rəhbərliyi asanlaşdırır.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Server Management Tool Çap get';
+$lang['print_server_management_tool'] = 'Server Management Tool Print';
+$lang['print_server_management_tool_help'] = 'Print Server idarə vasitədir daxil olmaq üçün linkə baxın.';
+$lang['print_server_management_tool_not_accessible'] = 'server çalışan zaman Print Server idarə vasitədir mövcud deyil';
