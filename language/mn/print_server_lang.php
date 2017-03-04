0406ef56c635838da7598a0dad12e137ae6223d5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:44:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Нарийвчилсан хэвлэх сервер програм сүлжээнд холбогдсон хэвлэгч дээр нь нийтлэг хэвлэх интерфэйсийг олгодог. Энэ нь LAN дээр үйлчлүүлэгч хэвлэх нөөцийг хуваалцах боломжийг олгодог, нэг төвлөрсөн үйлчилгээнд удирдлагыг хөнгөвчилдөг.';
+$lang['print_server_app_name'] = 'Нарийвчилсан Хэвлэх Серверүүд';
+$lang['print_server_go_to_management_tool'] = 'Server Management хэрэгсэл Хэвлэх рүү очих';
+$lang['print_server_management_tool'] = 'Server Management хэрэгсэл хэвлэнэ';
+$lang['print_server_management_tool_help'] = 'Хэвлэх Server удирдлагын хэрэгсэл хандах холбоосыг дагана уу.';
+$lang['print_server_management_tool_not_accessible'] = 'сервер ажиллаж байх үед нь хэвлэх сервер удирдлагын арга хэрэгсэл байхгүй бол';
