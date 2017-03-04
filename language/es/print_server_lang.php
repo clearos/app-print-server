0b65ea2d19429acb2ae78d7b3e517b72b0a9fec1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:57:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'La aplicación avanzada de servidor de impresión proporciona una interfaz común de impresión para impresoras de la red. Esto permite a los clientes de la LAN para compartir los recursos de impresión y facilita la administración en un servicio centralizado.';
+$lang['print_server_app_name'] = 'Servidor de impresión avanzada';
+$lang['print_server_go_to_management_tool'] = 'Ir a la Herramienta de impresión del servidor de administración';
+$lang['print_server_management_tool'] = 'Herramienta de impresión del servidor de administración';
+$lang['print_server_management_tool_help'] = 'Siga el enlace para acceder a la herramienta de administración del servidor de impresión.';
+$lang['print_server_management_tool_not_accessible'] = 'La herramienta de gestión de servidor de impresión no está disponible cuando el servidor no se está ejecutando';
