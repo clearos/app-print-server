118578e2331d4285c32d94d4efc91f2ea6986616
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:50:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'این نرم افزار پیشرفته چاپ و سرور یک رابط چاپ مشترک به پرینتر در شبکه فراهم می کند. این اجازه می دهد تا مشتریان بر روی شبکه محلی برای به اشتراک گذاشتن منابع چاپی و آسان می کند دولت را به یکی از خدمات متمرکز.';
+$lang['print_server_app_name'] = 'پیشرفته چاپ و سرور';
+$lang['print_server_go_to_management_tool'] = 'برو به چاپ ابزار مدیریت سرور';
+$lang['print_server_management_tool'] = 'چاپ ابزار مدیریت سرور';
+$lang['print_server_management_tool_help'] = 'دنبال لینک برای دسترسی به ابزار چاپ مدیریت سرور.';
+$lang['print_server_management_tool_not_accessible'] = 'ابزار مدیریت چاپ و سرور در دسترس نیست زمانی که سرور در حال اجرا نیست';
