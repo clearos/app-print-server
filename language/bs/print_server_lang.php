3c20625a19ce97e0f4de92625dec58592cde8a97
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:14:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Advanced Print Server aplikacija pruža zajednički ispis interfejs štampača na mrežu. To omogućava klijentima na LAN da dijele resurse ispis i olakšava administraciju u jednu centralizovanu usluga.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Idi na Print Management Tool Server';
+$lang['print_server_management_tool'] = 'Ispis Alat Management Server';
+$lang['print_server_management_tool_help'] = 'Pratite link za pristup alat Print Management Server.';
+$lang['print_server_management_tool_not_accessible'] = 'Alat za upravljanje Print Server nije dostupan kada server ne radi';
