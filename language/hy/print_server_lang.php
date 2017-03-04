2d9f83424d49cfa2fe850b163fff02e78e7b498d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:10:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Ընդլայնված Print Server ծրագիրը ապահովում է ընդհանուր տպագիր ինտերֆեյսի տպիչների վրա ցանցում. Սա թույլ է տալիս հաճախորդներին տեղեկացնում LAN է կիսել տպագիր ռեսուրսները եւ eases վարչարարության մեջ մեկ կենտրոնացված ծառայությունից:';
+$lang['print_server_app_name'] = 'Ընդլայնված Print Server';
+$lang['print_server_go_to_management_tool'] = 'Գնալ Տպել Server կառավարման գործիք';
+$lang['print_server_management_tool'] = 'Տպել Server կառավարման գործիք';
+$lang['print_server_management_tool_help'] = 'Հետեւեք հղումը մուտք գործելու Print Server կառավարման գործիք.';
+$lang['print_server_management_tool_not_accessible'] = 'Տպագիր Server կառավարման գործիք հասանելի չէ, երբ սերվերը չի առաջադրվել';
