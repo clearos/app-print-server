daaedbb49a1f9152b70c8fe4b97c69cf01e201b9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:16:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'အဆိုပါအဆင့်မြင့်ပရင့်ထုတ်ရန်ဆာဗာ app ကိုကွန်ရက်ပေါ်တွင်ပရင်တာမှတစ်ဘုံပုံနှိပ် interface ကိုပေးစွမ်းသည်။ ဒါကရှိနေရင် LAN ပေါ်မှ clients များပုံနှိပ်သယံဇာတများမျှဝေဖို့ခွင့်ပြုနှင့်တစ်ဗဟိုဝန်ဆောင်မှုသို့အုပ်ချုပ်ရေးလျှော့ချပေးခဲ့။';
+$lang['print_server_app_name'] = 'အဆင့်မြင့်ပရင့်ထုတ်ရန် Server ကို';
+$lang['print_server_go_to_management_tool'] = 'Server ကိုစီမံခန့်ခွဲမှု Tool ကို Print ကိုသွားပါ';
+$lang['print_server_management_tool'] = 'ပရင့်ထုတ်ရန် Server ကိုစီမံခန့်ခွဲမှု Tool ကို';
+$lang['print_server_management_tool_help'] = 'ထိုပရင့်ထုတ်ရန် Server ကိုစီမံခန့်ခွဲမှု tool ကိုဝင်ရောက်ဖို့ link ကိုလိုက်နာပါ။';
+$lang['print_server_management_tool_not_accessible'] = 'ဆာဗာ run မရသောအခါအဆိုပါပရင့်ထုတ်ရန် Server ကိုစီမံခန့်ခွဲမှု tool ကိုမရရှိနိုင်';
