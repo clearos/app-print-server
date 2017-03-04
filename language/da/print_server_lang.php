55eb6ba6238727a931a187daf001d7eb3fb5fa84
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:21:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Den avancerede printserver app giver en fælles print interface til printere på netværket. Dette gør det muligt klienter på LAN til at dele trykte ressourcer og letter administrationen i en centraliseret tjeneste.';
+$lang['print_server_app_name'] = 'Avanceret Print Server';
+$lang['print_server_go_to_management_tool'] = 'Gå til Print Server Management Tool';
+$lang['print_server_management_tool'] = 'Print Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Følg linket for at få adgang til Print Server ledelsesværktøj.';
+$lang['print_server_management_tool_not_accessible'] = 'Den printserver ledelsesværktøj er ikke tilgængelig, når serveren ikke kører';
