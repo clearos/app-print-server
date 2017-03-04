f5a65709a36f6fb98cfae1d7143f84ea28177493
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:22:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Advanced Print Server app annab ühise print liides võrgus printereid. See võimaldab klientidel LAN jagada print ressursse ja kiirendab manustamine ühte tsentraliseeritud teenust.';
+$lang['print_server_app_name'] = 'Täpsem printserver';
+$lang['print_server_go_to_management_tool'] = 'Mine Prindi Server Management Tool';
+$lang['print_server_management_tool'] = 'Trüki Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Jälgi lingil printimisserveri juhtimisvahend.';
+$lang['print_server_management_tool_not_accessible'] = 'Printimisserveri juhtimisvahend ei ole kättesaadav, kui server ei tööta';
