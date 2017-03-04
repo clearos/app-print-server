78ae9976e37bc7722c71b0218d2537a9969a73ef
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:32:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'The Advanced Print Server app afla sameiginlega prenta tengi til prentara á netinu. Þetta gerir viðskiptavinum á LAN til að deila prenta auðlindir og dregur gjöf í eitt miðlægt þjónustu.';
+$lang['print_server_app_name'] = 'Ítarlegri Prenta Server';
+$lang['print_server_go_to_management_tool'] = 'Fara til Prenta Server Management Tool';
+$lang['print_server_management_tool'] = 'Prenta Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Fylgdu tenglinum til að fá aðgang að Print Server stjórnun tól.';
+$lang['print_server_management_tool_not_accessible'] = 'The Print Server stjórnun tól er ekki í boði þegar þjóninn er ekki í gangi';
