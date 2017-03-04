033bfd314b16905257734802a1362884067a7b0c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:30:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Advanced Print Server aplikasi menyediakan antarmuka cetak umum untuk printer pada jaringan. Hal ini memungkinkan klien pada LAN untuk berbagi sumber daya cetak dan memudahkan administrasi ke dalam satu layanan terpusat.';
+$lang['print_server_app_name'] = 'Canggih Print Server';
+$lang['print_server_go_to_management_tool'] = 'Pergi untuk Cetak Manajemen Perangkat Server';
+$lang['print_server_management_tool'] = 'Cetak Manajemen Perangkat Server';
+$lang['print_server_management_tool_help'] = 'Ikuti link untuk mengakses alat manajemen Print Server.';
+$lang['print_server_management_tool_not_accessible'] = 'Alat manajemen Print Server tidak tersedia ketika server tidak berjalan';
