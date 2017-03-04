0b9b4a4eef53fc9be6f754d79a12d20e2d6514a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:08:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Awọn To ti ni ilọsiwaju Print Server app pese a wọpọ ta ni wiwo to atẹwe lori awọn nẹtiwọki. Eleyi gba ibara lori lan lati pin si ta oro ati eases isakoso sinu ọkan si aarin iṣẹ.';
+$lang['print_server_app_name'] = 'To ti ni ilọsiwaju Print Server';
+$lang['print_server_go_to_management_tool'] = 'Lọ si Print Server Management ọpa';
+$lang['print_server_management_tool'] = 'Sita Server Management ọpa';
+$lang['print_server_management_tool_help'] = 'Tẹle awọn ọna asopọ lati wọle si awọn Print Server isakoso ọpa.';
+$lang['print_server_management_tool_not_accessible'] = 'Awọn Print Server isakoso ọpa ni ko wa nigbati awọn olupin ti wa ni ko ti nṣiṣẹ';
