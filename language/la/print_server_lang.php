c6c46083a9bcf2ee0941c42f3bdc81829521387d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:38:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'A app suggero a communi server Advanced Print Print Printers in interface ut ipsum. Hoc permittit clients LAN ad participant in in una administratione Print centralized Service Resources et mitigavit.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Go Server ad Print Management Tool';
+$lang['print_server_management_tool'] = 'Print Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Sequi Management Print Server obvius ad instrumentum.';
+$lang['print_server_management_tool_not_accessible'] = 'Cum autem non competit ministris Print Server currit procuratio tool';
