51b2e60c710ebd1d9828f256996760fbeab61c3f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:28:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'The Advanced Print Server app samar da wani na kowa buga dubawa to firintocinku a kan hanyar sadarwa. Wannan damar abokan ciniki a kan LAN raba buga albarkatun da eases gwamnati a cikin daya Karkasa sabis.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Ka tafi zuwa ga Print Server Management Tool';
+$lang['print_server_management_tool'] = 'Print Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Bi mahada don samun damar Print Server management kayan aiki.';
+$lang['print_server_management_tool_not_accessible'] = 'The Print Server management kayan aiki ba samuwa a lõkacin da uwar garken ba a guje';
