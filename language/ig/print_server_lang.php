ec445016745300db517a80c79944439c42abe0a1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:32:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'The Advanced Bipute Server ngwa na-enye a nkịtị ebipụta interface ka na-ebi akwụkwọ na netwọk. Nke a na-enye ohere ahịa na LAN ịkọrọ ebipụta ihe onwunwe na eases ochichi n&#39;ime otu centralized ọrụ.';
+$lang['print_server_app_name'] = 'Advanced Bipute Server';
+$lang['print_server_go_to_management_tool'] = 'Gaa Bipute Server Management Ngwá Ọrụ';
+$lang['print_server_management_tool'] = 'Bipute Server Management Ngwá Ọrụ';
+$lang['print_server_management_tool_help'] = 'Soro njikọ ka ịnweta Bipute Server management ngwá ọrụ.';
+$lang['print_server_management_tool_not_accessible'] = 'The Bipute Server management ngwá ọrụ a adịghị mgbe ihe nkesa na-adịghị na-agba ọsọ';
