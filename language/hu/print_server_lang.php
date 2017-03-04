293b86951257cb9fd4bfd98bd74aab7e5fde4a0a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:29:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Az Advanced Print Server alkalmazás biztosítja a közös nyomtatási felület a hálózati nyomtatókat. Ez lehetővé teszi, hogy az ügyfelek a LAN megosztani nyomtatott források, megkönnyíti adminisztráció egyetlen központi szolgáltató.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Tovább a Print Server Management Tool';
+$lang['print_server_management_tool'] = 'Print Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Kövesse a linket, hogy hozzáférjen a Print Server eszköz.';
+$lang['print_server_management_tool_not_accessible'] = 'A Print Server eszköz nem áll rendelkezésre, ha a szerver nem fut';
