e976bebd6d6765e17ceb9730c6ed253c3623e877
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:57:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'The Advanced Print Server app e fana ka tlhaloso e tloaelehileng условия: segokanyimmediamentsi sa sebolokigolo ho bahatisi tsa leqhubu eo. Sena se nolofalletsa bareki ka Lan ho kopanela mehlodi ya ho hatisa le ho eases tsamaiso qobella &#39;ngoe gare wa.';
+$lang['print_server_app_name'] = 'E tsoetseng pele Print Server';
+$lang['print_server_go_to_management_tool'] = 'Eya ho Print Server Management Tool';
+$lang['print_server_management_tool'] = 'Hatisa Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Follow kgokahanyo ho fihlella Print taolo ya Server sesebelisoa.';
+$lang['print_server_management_tool_not_accessible'] = 'The sesebelisoa taolo ya Print Server e fumanehe ha seva e sa matha';
