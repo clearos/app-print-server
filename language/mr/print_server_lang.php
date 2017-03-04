79f759ea1cfe41719e09d582d1df136ad89e7146
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:45:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'प्रगत प्रिंट सर्व्हर अॅप नेटवर्क प्रिंटर एक सामान्य प्रिंट संवाद पुरवतो. या लॅन क्लायंट प्रिंट संसाधने सामायिक करण्याची परवानगी देते आणि एक मध्यवर्ती सेवा प्रशासन धाव.';
+$lang['print_server_app_name'] = 'प्रगत प्रिंट सर्व्हर';
+$lang['print_server_go_to_management_tool'] = 'प्रिंट सर्व्हर व्यवस्थापन साधन जा';
+$lang['print_server_management_tool'] = 'प्रिंट करा सर्व्हर व्यवस्थापन साधन';
+$lang['print_server_management_tool_help'] = 'प्रिंट सर्व्हर व्यवस्थापन साधन प्रवेश करण्यासाठी दुवा अनुसरण.';
+$lang['print_server_management_tool_not_accessible'] = 'सर्व्हर चालवत नाही तेव्हा प्रिंट सर्व्हर व्यवस्थापन उपकरण उपलब्ध नाही';
