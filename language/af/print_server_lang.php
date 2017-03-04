0bb406d2735468c2fa17178942712459451c5a17
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:08:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Die Gevorderde Print Server app bied &#39;n gemeenskaplike druk koppelvlak drukkers op die netwerk. Dit laat kliënte op die LAN te druk hulpbronne te deel en vergemaklik administrasie in een gesentraliseerde diens.';
+$lang['print_server_app_name'] = 'Gevorderde Print Server';
+$lang['print_server_go_to_management_tool'] = 'Gaan na Druk Server Management Tool';
+$lang['print_server_management_tool'] = 'Druk Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Volg die skakel om toegang te verkry tot die afdruk server beheer instrument.';
+$lang['print_server_management_tool_not_accessible'] = 'Die Print Server beheer instrument is nie beskikbaar wanneer die bediener nie aktief';
