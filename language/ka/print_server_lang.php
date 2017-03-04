97f54a19c8805b6279a9740a32d16eb5e9b3b679
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:35:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'გაფართოებული Print Server app უზრუნველყოფს საერთო ინტერფეისი პრინტერები ქსელში. ეს საშუალებას აძლევს კლიენტებს LAN გაუზიაროს ბეჭდვითი რესურსების და აადვილებს ადმინისტრაციის ერთ ცენტრალიზებული მომსახურება.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'წავიდეს ბეჭდვა Server მართვის Tool';
+$lang['print_server_management_tool'] = 'Print Server მართვის Tool';
+$lang['print_server_management_tool_help'] = 'ბმულზე წვდომის Print Server მართვის ინსტრუმენტი.';
+$lang['print_server_management_tool_not_accessible'] = 'ბეჭდვის სერვერზე მართვის ინსტრუმენტი არის შესაძლებელი, როდესაც სერვერზე არ არის გაშვებული';
