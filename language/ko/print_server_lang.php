12e9b10b9472fa22a5b1c0b6f0e2e28839eb10b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:38:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = '고급 인쇄 서버 응용 프로그램은 네트워크에있는 프린터에 대한 공통 인쇄 인터페이스를 제공합니다. 이것은 LAN에 클라이언트가 인쇄 리소스를 공유 할 수 있도록 하나의 중앙 집중식 서비스로 관리를 용이하게합니다.';
+$lang['print_server_app_name'] = '고급 인쇄 서버';
+$lang['print_server_go_to_management_tool'] = '서버 관리 도구를 인쇄로 이동';
+$lang['print_server_management_tool'] = '서버 관리 도구를 인쇄';
+$lang['print_server_management_tool_help'] = '인쇄 서버 관리 도구에 액세스 할 수있는 링크를 따르십시오.';
+$lang['print_server_management_tool_not_accessible'] = '서버가 실행되지 않을 때 인쇄 서버 관리 도구를 사용할 수 없습니다';
