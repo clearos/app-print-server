f7a96fac6d44ec85b981bb91c62967955f87fca3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:20:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Rozšířené aplikace Print Server poskytuje společné tiskové rozhraní do tiskárny v síti. To umožňuje klientům na síti LAN sdílet tiskové prostředky a zjednodušují správu do jedné centralizované služby.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Přejděte k tisku nástroj Správa serveru';
+$lang['print_server_management_tool'] = 'Vytisknout nástroj Správa serveru';
+$lang['print_server_management_tool_help'] = 'Klikněte na odkaz pro přístup nástroj pro správu tiskového serveru.';
+$lang['print_server_management_tool_not_accessible'] = 'Nástroj pro správu Print Server není k dispozici, pokud není spuštěn server';
