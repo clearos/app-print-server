571bf17ea6ce0e9f64b4a04e7fb9f6d7b78db5dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:52:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Advanced App Print Server oferă o interfață de imprimare comună pentru imprimante de pe rețea. Acest lucru permite clienților de pe LAN pentru a partaja resursele de imprimare și să faciliteze administrarea într-un singur serviciu centralizat.';
+$lang['print_server_app_name'] = 'Avansat Print Server';
+$lang['print_server_go_to_management_tool'] = 'Mergi la Print Server Management Tool';
+$lang['print_server_management_tool'] = 'Instrumentul de Imprimare Management Server';
+$lang['print_server_management_tool_help'] = 'Urmați link-ul pentru a accesa instrumentul de gestionare a serverului de imprimare.';
+$lang['print_server_management_tool_not_accessible'] = 'Instrumentul de gestionare a serverului de imprimare nu este disponibilă atunci când serverul nu se execută';
