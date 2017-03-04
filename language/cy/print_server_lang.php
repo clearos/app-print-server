8b89bd50133531aba9a8a5234a3880739a94acbe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:06:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Mae&#39;r Uwch app Print Gweinydd darparu rhyngwyneb print gyffredin i argraffyddion ar y rhwydwaith. Mae hyn yn caniatáu cleientiaid ar y LAN i rannu adnoddau print ac yn hwyluso gweinyddu i mewn i un gwasanaeth canolog.';
+$lang['print_server_app_name'] = 'Uwch Print Gweinydd';
+$lang['print_server_go_to_management_tool'] = 'Ewch i Argraffu Offeryn Rheoli Gweinydd';
+$lang['print_server_management_tool'] = 'Argraffu Offeryn Rheoli Gweinydd';
+$lang['print_server_management_tool_help'] = 'Dilynwch y ddolen i gael mynediad i&#39;r teclyn rheoli Gweinydd Argraffu.';
+$lang['print_server_management_tool_not_accessible'] = 'Nid yw&#39;r offeryn rheoli Print Gweinydd ar gael pan na fydd y gweinydd yn rhedeg';
