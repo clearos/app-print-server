3ba67adcef352fbf019de05ee06d12324b4eec8f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:59:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Advanced Print Server programu hutoa kawaida magazeti interface kwa Printers kwenye mtandao. Hii inaruhusu wateja juu ya LAN ya kugawana rasilimali magazeti na eases utawala katika huduma moja kati.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Kwenda Magazeti Server Management Tool';
+$lang['print_server_management_tool'] = 'Magazeti Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Kufuata kiungo kwa upatikanaji Print usimamizi Server chombo.';
+$lang['print_server_management_tool_not_accessible'] = 'chombo cha usimamizi wa Magazeti Server haipatikani wakati server si mbio';
