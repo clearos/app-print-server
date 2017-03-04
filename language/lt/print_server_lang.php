07fc6da94ebf5714131b7bbfb2add54a701dc307
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:40:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Išplėstinė Spausdinti serverio programa suteikia bendrą spausdinimo sąsaja su spausdintuvais tinkle. Tai leidžia klientams apie LAN pasidalinti spausdinimo išteklius ir palengvina administravimą į vieną centralizuotą paslaugų.';
+$lang['print_server_app_name'] = 'Išplėstinė Print Server';
+$lang['print_server_go_to_management_tool'] = 'Eiti į spausdinimo serverio valdymo įrankis';
+$lang['print_server_management_tool'] = 'Spausdinti serverio valdymo įrankį';
+$lang['print_server_management_tool_help'] = 'Sekite nuorodą prieiti prie Spausdinti serverio valdymo įrankį.';
+$lang['print_server_management_tool_not_accessible'] = 'Spausdinimo serverio valdymo įrankį nėra kai serveris nėra paleistas';
