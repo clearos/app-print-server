6147f4af13ce8eef692dc52bbc5f617b0cba48b9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:23:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Advanced Print Server sovellus tarjoaa yhteisen tulosta rajapinta verkon tulostimet. Tämä mahdollistaa asiakkaille lähiverkon jakaa tulostaa resursseja ja helpottaa hallinnon yhdeksi keskitetty palvelu.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Mene Print Server Management Tool';
+$lang['print_server_management_tool'] = 'Tulosta Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Seuraa linkin kautta Print Server hallintatyökalu.';
+$lang['print_server_management_tool_not_accessible'] = 'Print Server hallinnan työkalu ei ole käytettävissä, kun palvelin ei ole käynnissä';
