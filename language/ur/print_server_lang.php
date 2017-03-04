07a03ce11637bcb396cd4c7fc7fbccc13b177973
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:04:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'اعلی درجے پرنٹ سرور اپلی کیشن نیٹ ورک پر، پرنٹروں کے لئے ایک عام پرنٹ انٹرفیس فراہم کرتا ہے. یہ LAN پر کلائنٹس تعداد وسائل کا اشتراک کرنے کی اجازت دیتا ہے اور ایک مرکزی سروس میں انتظامیہ پابندیاں نرم کر دیں.';
+$lang['print_server_app_name'] = 'اعلی درجے پرنٹ سرور';
+$lang['print_server_go_to_management_tool'] = 'سرور کے انتظام کا آلہ پرنٹ پر جائیں';
+$lang['print_server_management_tool'] = 'پرنٹ سرور کے انتظام آلے';
+$lang['print_server_management_tool_help'] = 'پرنٹ سرور کے انتظام کے آلے تک رسائی حاصل کرنے کے لئے لنک پر عمل کریں.';
+$lang['print_server_management_tool_not_accessible'] = 'جب سرور نہیں چل رہا ہے پرنٹ سرور کے انتظام کا آلہ دستیاب نہیں ہے';
