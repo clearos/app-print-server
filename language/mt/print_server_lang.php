fb71ef99ab5271673969205d99c66a4ba407a552
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:46:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Il Avvanzata app Stampa Server jipprovdi interface istampar komuni għall-printers fuq in-netwerk. Dan jippermetti lill-klijenti fuq il-LAN biex jinqasmu riżorsi istampar u ttaffi l-amministrazzjoni fis-servizz ċentralizzat wieħed.';
+$lang['print_server_app_name'] = 'Avvanzata Stampa Server';
+$lang['print_server_go_to_management_tool'] = 'Mur fil Stampa Server Ġestjoni Għodda';
+$lang['print_server_management_tool'] = 'Stampa Server Ġestjoni Għodda';
+$lang['print_server_management_tool_help'] = 'Segwi l-link għall-aċċess l-għodda Stampa ġestjoni Server.';
+$lang['print_server_management_tool_not_accessible'] = 'L-għodda ta &#39;ġestjoni Stampa Server mhuwiex disponibbli meta l-server ma tkunx qed taħdem';
