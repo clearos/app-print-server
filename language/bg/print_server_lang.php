96101fda82336489270f4f26b98c86764695144b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:15:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Приложението Advanced Print Server осигурява обща печат интерфейс за принтери в мрежата. Това дава възможност на клиентите в локалната мрежа да споделят печат ресурси и облекчава администрация в една централизирана услуга.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Отиди Печат Tool Management Server';
+$lang['print_server_management_tool'] = 'Печат Tool Management Server';
+$lang['print_server_management_tool_help'] = 'Следвайте връзката за достъп до инструмента за управление на сървър за печат на.';
+$lang['print_server_management_tool_not_accessible'] = 'Инструментът за управление на сървър за печат не е на разположение, когато сървърът не работи';
