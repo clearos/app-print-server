01a67233a2f9ea9a2cc027870e1101b0e06b99ed
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:39:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'ການ app Print Server ແບບພິເສດໃຫ້ມີການໂຕ້ຕອບການພິມທົ່ວໄປທີ່ຈະພິມໃນເຄືອຂ່າຍ. ນີ້ອະນຸຍາດໃຫ້ລູກຄ້າກ່ຽວກັບລະບົບ LAN ທີ່ຈະແບ່ງປັນຊັບພະຍາກອນການພິມແລະຫຼຸດຜ່ອນການບໍລິຫານເຂົ້າໄປໃນການບໍລິການສ່ວນກາງຫນຶ່ງ.';
+$lang['print_server_app_name'] = 'Server ພິມກ້າວຫນ້າທາງດ້ານ';
+$lang['print_server_go_to_management_tool'] = 'ໄປທີ່ Print ເຄື່ອງມືການຈັດການ Server';
+$lang['print_server_management_tool'] = 'ພິມເຄື່ອງມືຈັດການ Server';
+$lang['print_server_management_tool_help'] = 'ປະຕິບັດຕາມການເຊື່ອມຕໍ່ໃນການເຂົ້າເຖິງການບໍລິຫານ Server ພິມ.';
+$lang['print_server_management_tool_not_accessible'] = 'ເຄື່ອງມືການຈັດການ Print Server ບໍ່ສາມາດໃຊ້ໃນເວລາທີ່ເຄື່ອງແມ່ຂ່າຍແມ່ນບໍ່ໄດ້ເຮັດວຽກ';
