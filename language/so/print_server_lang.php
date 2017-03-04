42b9f71f92135ea0e64027e93e60bbaf2369bab0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:56:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'The app Print Server Advanced bixisaa interface a print caadi ah in madbacadaha on shabakada. Tani waxay u ogolaaneysaa macaamiisha on LAN ah si ay ula wadaagaan khayraadka print iyo sahlaysaa Maamulka galay mid ka mid adeegga dhexe.';
+$lang['print_server_app_name'] = 'Print Server Advanced';
+$lang['print_server_go_to_management_tool'] = 'Tag Print Server Management Tool';
+$lang['print_server_management_tool'] = 'Print Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Raac link si ay u helaan qalab Print maamulka Server ah.';
+$lang['print_server_management_tool_not_accessible'] = 'qalab maamulka Print Server The aan la heli karin marka server aan la ordaya';
