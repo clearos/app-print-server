2f8f101ba209125685878a4d32f5495a69832e13
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:41:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Advanced Print Server app nodrošina vienotu drukas saskarni printeriem tīklā. Tas ļauj klientiem par LAN dalīties drukas resursus un atvieglo administrāciju vienā centralizētā ekspluatācijā.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Doties uz Print Server Management Tool';
+$lang['print_server_management_tool'] = 'Drukāt Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Sekojiet uz saites, lai piekļūtu Print Server pārvaldības rīks.';
+$lang['print_server_management_tool_not_accessible'] = 'Print Server vadības rīks nav pieejama, ja serveris nedarbojas';
