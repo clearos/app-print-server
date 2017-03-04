03197292db732c45eb305d4724d5becfcc532adf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:31:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Soláthraíonn an app Casta Freastalaí Priontáil comhéadan phriontáil coiteann do clódóirí ar an ngréasán. Ligeann sé seo do chliaint ar an LAN a chomhroinnt acmhainní cló agus eases riaracháin i seirbhís láraithe amháin.';
+$lang['print_server_app_name'] = 'Ard Freastalaí Priontáil';
+$lang['print_server_go_to_management_tool'] = 'Téigh go dtí Priontáil Uirlis Bainistíochta Freastalaí';
+$lang['print_server_management_tool'] = 'Priontáil Uirlis Bainistíochta Freastalaí';
+$lang['print_server_management_tool_help'] = 'Lean an nasc chun teacht ar an uirlis bainistíochta Freastalaí Priontáil.';
+$lang['print_server_management_tool_not_accessible'] = 'Níl an uirlis bainistíochta Freastalaí Priontáil fáil nuair nach bhfuil an freastalaí ag rith';
