457632c83e48e2f53be17b1262edba4313ad3897
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:03:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Ang Advanced Print Server app ay nagbibigay ng isang pangkaraniwang interface print sa printer sa network. Ito ay nagpapahintulot sa mga kliyente sa LAN na ibahagi resources print at eases pangangasiwa sa isang sentralisadong serbisyo.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Pumunta sa I-print ang Server Management Tool';
+$lang['print_server_management_tool'] = 'I-print ang Server Management Tool';
+$lang['print_server_management_tool_help'] = 'Sundin ang mga link upang ma-access ang Print Server management tool.';
+$lang['print_server_management_tool_not_accessible'] = 'Ang pamamahala Print Server tool ay hindi magagamit kapag ang server ay hindi tumatakbo';
