e11049bd10db9667bbc086026abee141ea4f528c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:27:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'App a avanse Enprime sèvè bay yon koòdone ekri an lèt detache komen nan imprimantes sou rezo a. Sa a pèmèt kliyan sou LAN lan yo pataje resous enprime ak facility administrasyon nan yon sèl sèvis santralize.';
+$lang['print_server_app_name'] = 'Avanse Enprime sèvè';
+$lang['print_server_go_to_management_tool'] = 'Ale nan Enprime sèvè Jesyon Zouti';
+$lang['print_server_management_tool'] = 'Enprime sèvè Jesyon Zouti';
+$lang['print_server_management_tool_help'] = 'Swiv lyen ki jwenn aksè nan zouti a sèvè jesyon Enprime.';
+$lang['print_server_management_tool_not_accessible'] = 'Zouti a jesyon Enprime sèvè a pa disponib lè sèvè a pa ap kouri';
