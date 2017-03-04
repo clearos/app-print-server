79e21c014fa3ff280b2b8fa5683caf8bd8317d00
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:47:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'उन्नत सर्भरमुद्रणगर्नुहोस् अनुप्रयोग नेटवर्कमा प्रिंटरको एक साधारण मुद्रण इन्टरफेस प्रदान गर्दछ। यो लैन मा ग्राहकहरु मुद्रण स्रोतहरू साझेदारी गर्न अनुमति दिन्छ र एक केंद्रीकृत सेवा मा प्रशासन eases।';
+$lang['print_server_app_name'] = 'विकसित सर्भरमुद्रणगर्नुहोस्';
+$lang['print_server_go_to_management_tool'] = 'सर्भर व्यवस्थापन उपकरण मुद्रण जानुहोस्';
+$lang['print_server_management_tool'] = 'सर्भर व्यवस्थापन उपकरण छाप्न';
+$lang['print_server_management_tool_help'] = 'मुद्रण सर्भर व्यवस्थापन उपकरण पहुँच लिङ्क पालना गर्नुहोस्।';
+$lang['print_server_management_tool_not_accessible'] = 'मुद्रण सर्भर व्यवस्थापन उपकरण उपलब्ध सर्भर छैन चलिरहेको बेला छ';
