58b91342c5c391903f7e4407ae3e1b99217682f6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:12:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'The aurreratua Print Server aplikazioa komun inprimatu sarean inprimagailuak interfaze bat eskaintzen du. Horrek aukera ematen die LAN bezero inprimatu baliabideak partekatzeko eta administrazio eases zerbitzu zentralizatu bat sartu.';
+$lang['print_server_app_name'] = 'Aurreratua Print Server';
+$lang['print_server_go_to_management_tool'] = 'Joan Server Kudeaketa Erremintaren inprimatu';
+$lang['print_server_management_tool'] = 'Inprimatzeko zerbitzaria kudeatzeko tresna';
+$lang['print_server_management_tool_help'] = 'Jarraitu esteka Print Server kudeaketa tresna sartzeko.';
+$lang['print_server_management_tool_not_accessible'] = 'Print Server kudeaketa tresna ez dago eskuragarri zerbitzaria ez dago martxan';
