cfe540831caa5e13073f49918e36cc0698f3ecb3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:01:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Дар барнома Advanced Print Server интерфейси чоп умумӣ ба чопгарҳои дар шабакаи таъмин менамояд. Ин имкон медиҳад, муштариён дар шабакаи маҳаллӣ барои мубодилаи захираҳои чопӣ ва eases идоракунии як хидмати мутамарказ.';
+$lang['print_server_app_name'] = 'Advanced Чоп Server';
+$lang['print_server_go_to_management_tool'] = 'Бирав ба чоп Tool идораи Server';
+$lang['print_server_management_tool'] = 'Чоп Tool идораи Server';
+$lang['print_server_management_tool_help'] = 'Пайравӣ пайванд дастрасӣ ба воситаи Print идоракунии Server.';
+$lang['print_server_management_tool_not_accessible'] = 'Воситаи идоракунии Print Server дастрас нест чун сервер иҷро нашуда';
