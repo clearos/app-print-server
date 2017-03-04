8c483c5ecc4926237d64b2712bd1ca57680a9351
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:21:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'La Advanced Print Server app provizas komunan presaĵo interfaco por printiloj sur la reto. Tio permesas klientojn sur la LAN kunhavigi presitaj rimedoj kaj faciligas administradon en unu centralizita servo.';
+$lang['print_server_app_name'] = 'Altnivelaj Presi Servilo';
+$lang['print_server_go_to_management_tool'] = 'Iru Print Server Management Ilo';
+$lang['print_server_management_tool'] = 'Presi Servilo Management Ilo';
+$lang['print_server_management_tool_help'] = 'Sekvu la ligon por aliri la Presi Servilo mastrumado ilo.';
+$lang['print_server_management_tool_not_accessible'] = 'La Presi Servilo mastrumado ilo ne estas disponebla kiam la servilo ne funkcias';
