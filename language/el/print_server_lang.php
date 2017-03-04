007a61420ef40c362d8c93cfec957872537e8328
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:26:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Η Advanced App Print Server παρέχει μια κοινή διεπαφή εκτύπωση σε εκτυπωτές στο δίκτυο. Αυτό επιτρέπει στους πελάτες στο LAN να μοιράζονται τους πόρους εκτύπωσης και διευκολύνει τη χορήγηση σε μία κεντρική υπηρεσία.';
+$lang['print_server_app_name'] = 'Σύνθετη Print Server';
+$lang['print_server_go_to_management_tool'] = 'Πηγαίνετε στο Print εργαλείο Management Server';
+$lang['print_server_management_tool'] = 'Εκτύπωση Εργαλείο Management Server';
+$lang['print_server_management_tool_help'] = 'Ακολουθήστε το σύνδεσμο για να αποκτήσετε πρόσβαση στο εργαλείο Εκτύπωση διαχείρισης διακομιστή.';
+$lang['print_server_management_tool_not_accessible'] = 'Το εργαλείο διαχείρισης server εκτύπωσης δεν είναι διαθέσιμη όταν ο διακομιστής δεν εκτελείται';
