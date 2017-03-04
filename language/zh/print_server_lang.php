22f116737d2098f592b1901ca5b2bd15817610f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:18:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = '高级打印服务器的应用程序提供了一个通用的打印接口，网络上的打印机。 这使得局域网上的客户端共享打印资源，简化管理到一个集中的服务。';
+$lang['print_server_app_name'] = '高级打印服务器';
+$lang['print_server_go_to_management_tool'] = '转到打印服务器管理工​​具';
+$lang['print_server_management_tool'] = '打印服务器管理工​​具';
+$lang['print_server_management_tool_help'] = '按照访问打印服务器管理工​​具的链接。';
+$lang['print_server_management_tool_not_accessible'] = '打印服务器的管理工具不可用在服务器不运行时';
