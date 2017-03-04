9d6c3f72d872a17fdc4a1dbf987b36f5e26d5983
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:13:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'উন্নত প্রিন্ট সার্ভার অ্যাপ্লিকেশন নেটওয়ার্কে প্রিন্টার একটি সাধারণ মুদ্রণ ইন্টারফেস উপলব্ধ. এই ল্যান ক্লায়েন্ট মুদ্রণ সম্পদ ভাগ করে দেয় এবং এক কেন্দ্রীভূত সেবা মধ্যে প্রশাসন eases.';
+$lang['print_server_app_name'] = 'উন্নত প্রিন্ট সার্ভার';
+$lang['print_server_go_to_management_tool'] = 'মুদ্রণ করার জন্য সার্ভার ম্যানেজমেন্ট টুল যান';
+$lang['print_server_management_tool'] = 'প্রিন্ট সার্ভার ম্যানেজমেন্ট টুল';
+$lang['print_server_management_tool_help'] = 'প্রিন্ট সার্ভার ম্যানেজমেন্ট টুল অ্যাক্সেস লিঙ্কটি অনুসরণ করুন.';
+$lang['print_server_management_tool_not_accessible'] = 'যখন সার্ভার চলছে না প্রিন্ট সার্ভার ম্যানেজমেন্ট টুল উপলব্ধ নয়';
