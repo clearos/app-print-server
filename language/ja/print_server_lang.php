b203abbb1267153c3d7f1ddf894b9399b7f193cf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:34:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = '高度なプリントサーバアプリは、ネットワーク上のプリンタに共通の印刷インタフェースを提供します。 これは、LAN上のクライアントが印刷リソースを共有することを可能にし、1集中型サービスへの投与が容易になります。';
+$lang['print_server_app_name'] = '高度なプリントサーバー';
+$lang['print_server_go_to_management_tool'] = 'サーバー管理ツールを印刷する戻ります';
+$lang['print_server_management_tool'] = 'プリントサーバー管理ツール';
+$lang['print_server_management_tool_help'] = 'プリントサーバー管理ツールにアクセスするためのリンクをクリックしてください。';
+$lang['print_server_management_tool_not_accessible'] = 'サーバーが実行されていない場合、プリントサーバー管理ツールが利用できません';
