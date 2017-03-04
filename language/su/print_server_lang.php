f6c75c9875f0a674a4a90313b5b8dd87f50179a9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:58:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Nu Advanced Print Server app nyadiakeun interface print ilahar printer dina jaringan. Hal ieu ngamungkinkeun klien dina LAN pikeun ngabagikeun daya print jeung eases administrasi ka hiji layanan terpusat.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Pindah ka Yuyun Management Alat Server';
+$lang['print_server_management_tool'] = 'Nyitak Management Alat Server';
+$lang['print_server_management_tool_help'] = 'Turutan link pikeun ngakses alat manajemén Print Server.';
+$lang['print_server_management_tool_not_accessible'] = 'Alat manajemén Print Server teu sadia sawaktos server teu ngajalankeun';
