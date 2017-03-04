6c714b6083e3e6030f098d4bc6498811061a0a3b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:03:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Gelişmiş Yazdırma Sunucusu uygulaması ağdaki yazıcıların ortak bir baskı arabirimi sağlar. Bu LAN üzerindeki istemcilerin baskı kaynaklarını paylaşmak için izin verir ve bir merkezi hizmete yönetimini kolaylaştırır.';
+$lang['print_server_app_name'] = 'Gelişmiş Baskı Sunucusu';
+$lang['print_server_go_to_management_tool'] = 'Sunucu Yönetimi Aracı Baskı git';
+$lang['print_server_management_tool'] = 'Sunucu Yönetimi Aracı yazdır';
+$lang['print_server_management_tool_help'] = 'Print Server yönetim aracı erişmek için bağlantıyı takip edin.';
+$lang['print_server_management_tool_not_accessible'] = 'Sunucu çalışmadığı zaman Print Server yönetim aracı kullanılamaz';
