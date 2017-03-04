7c8dc579c70715c1c58f311f4babf25e615cf271
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:25:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Die Advanced Print Server App bietet eine gemeinsame Druck Schnittstelle zu Druckern im Netzwerk. Dies ermöglicht es Kunden auf der LAN-Druckressourcen zu teilen und erleichtert Verwaltung in einem zentralen Service.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Zum Server-Management-Tool zu drucken';
+$lang['print_server_management_tool'] = 'Druckserver-Management-Tool';
+$lang['print_server_management_tool_help'] = 'Folgen Sie dem Link, um den Druckserver-Management-Tool zugreifen.';
+$lang['print_server_management_tool_not_accessible'] = 'Der Druckserver-Management-Tool ist nicht verfügbar, wenn der Server nicht läuft';
