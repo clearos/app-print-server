061af29724ca5d29ae1eda10be7cad26a2b6a61f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:17:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'The mwaukadauloZida Sindikizani Server app amapereka wamba kusindikiza mawonekedwe kwa osindikiza pa maukonde. Zimenezi zimathandiza makasitomala pa LAN kugawana chuma kusindikiza ndi eases makonzedwe mu utumiki wina centralized.';
+$lang['print_server_app_name'] = 'Zotsogola Sindikizani Server';
+$lang['print_server_go_to_management_tool'] = 'Pitani ku Sindikizani Server Management Chida';
+$lang['print_server_management_tool'] = 'Kusindikiza Server Management Chida';
+$lang['print_server_management_tool_help'] = 'Kutsatira kugwirizana kuti kulumikiza Sindikizani kasamalidwe Server chida.';
+$lang['print_server_management_tool_not_accessible'] = 'The chida kasamalidwe Sindikizani Server palibe pamene makina si kuthamanga';
