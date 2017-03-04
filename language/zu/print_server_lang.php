9e09599114a1eaff7b069cf807db322ed0cfe589
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:08:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Uhlelo Advanced lokusebenza Print eSeva ihlinzeka evamile ukuphrinta isikhombikubona abanyathelisi on inethiwekhi. Lokhu kuvumela amaklayenti LAN ukwabelana ukuphrinta izinsiza kanye esithe xaxa ngobukhulu nokuphathwa zibe munye service eyodwa.';
+$lang['print_server_app_name'] = 'Advanced Phrinta Server';
+$lang['print_server_go_to_management_tool'] = 'Iya ku Phrinta Management Ithuluzi Server';
+$lang['print_server_management_tool'] = 'Phrinta Management Ithuluzi Server';
+$lang['print_server_management_tool_help'] = 'Landela isixhumanisi ukuze ufinyelele ithuluzi Print Server.';
+$lang['print_server_management_tool_not_accessible'] = 'Ithuluzi ukuphathwa Print Iseva ayitholakali uma iseva is hhayi egijima';
