f32c88305d23a5a97edbe024cc9a75ccbf41a4d5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:05:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Ilg&#39;or Qog&#39;ozga chiqarish Server ilova tarmoq ustida printerlar uchun umumiy bosma interfeysi beradi. Bu LAN dasturlari, bosma resurslarini almashish uchun imkon beradi va bir markazlashtirilgan xizmatga boshqaruvini engillashtiradi.';
+$lang['print_server_app_name'] = 'Ilg&#39;or Qog&#39;ozga chiqarish Server';
+$lang['print_server_go_to_management_tool'] = 'Server Management asbobini chop o&#39;tish';
+$lang['print_server_management_tool'] = 'Server Management asbobini chop';
+$lang['print_server_management_tool_help'] = 'Chop etish uchun Server boshqaruv vositasi kirish uchun bog&#39;dan rioya qiling.';
+$lang['print_server_management_tool_not_accessible'] = 'server va hkz vaqt Print Server boshqaruv vositasi mavjud emas';
