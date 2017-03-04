55413bd63b70273105af7424a9e32c0bc88bec07
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:04:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Розширене додаток Print Server забезпечує загальний інтерфейс друку для принтерів в мережі. Це дозволяє клієнтам в локальній мережі для обміну ресурсами друку і спрощує управління в одне централізоване обслуговування.';
+$lang['print_server_app_name'] = 'Advanced Server друку';
+$lang['print_server_go_to_management_tool'] = 'Перейти до друку інструмент управління сервером,';
+$lang['print_server_management_tool'] = 'Друк інструмент управління сервером,';
+$lang['print_server_management_tool_help'] = 'Перейдіть по посиланню, щоб отримати доступ до інструмента друку для управління сервером.';
+$lang['print_server_management_tool_not_accessible'] = 'Засіб управління сервера друку недоступний, якщо сервер не працює';
