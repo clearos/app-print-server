2e7132e9dbdcafffa1552082353cde11fa9fbaf7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:09:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Aplikacioni i avancuar Print Server siguron një ndërfaqe të përbashkët të shtypura në printer në rrjet. Kjo i lejon klientëve në LAN për të ndarë burimet e shtypura dhe lehtëson administrimin në një shërbim të centralizuar.';
+$lang['print_server_app_name'] = 'Avancuar Print Server';
+$lang['print_server_go_to_management_tool'] = 'Shko tek Print Server Management Tool';
+$lang['print_server_management_tool'] = 'Print Server Menaxhimit Tool';
+$lang['print_server_management_tool_help'] = 'Ndiqni lidhjen për të hyrë në mjet Print Server menaxhimit.';
+$lang['print_server_management_tool_not_accessible'] = 'Mjet i menaxhimit Print Server nuk është në dispozicion, kur serveri nuk është i rrjedhshëm';
