5a14ed3a5361a9bef46a27b16272c1d11ee7d16b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:49:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'ਤਕਨੀਕੀ ਪ੍ਰਿੰਟ ਸਰਵਰ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਨੈੱਟਵਰਕ &#39;ਤੇ ਪ੍ਰਿੰਟਰ ਨੂੰ ਇੱਕ ਆਮ ਪ੍ਰਿੰਟ ਇੰਟਰਫੇਸ ਪ੍ਰਦਾਨ ਕਰਦਾ ਹੈ. ਇਹ LAN ਉੱਪਰ ਗਾਹਕ ਪ੍ਰਿੰਟ ਸਰੋਤ ਸ਼ੇਅਰ ਕਰਨ ਲਈ ਸਹਾਇਕ ਹੈ ਅਤੇ ਇੱਕ ਕਦਰੀ ਸੇਵਾ ਵਿੱਚ ਪ੍ਰਸ਼ਾਸਨ ਸੰਚਾਰ ਵਿੱਚ ਹੈ.';
+$lang['print_server_app_name'] = 'ਤਕਨੀਕੀ ਪ੍ਰਿੰਟ ਸਰਵਰ';
+$lang['print_server_go_to_management_tool'] = 'ਪ੍ਰਿੰਟ ਕਰਨ ਲਈ ਸਰਵਰ ਪਰਬੰਧਨ ਸੰਦ ਤੇ ਜਾਓ';
+$lang['print_server_management_tool'] = 'ਪ੍ਰਿੰਟ ਸਰਵਰ ਪਰਬੰਧਨ ਸੰਦ';
+$lang['print_server_management_tool_help'] = 'ਪ੍ਰਿੰਟ ਸਰਵਰ ਪਰਬੰਧਨ ਸੰਦ ਨੂੰ ਪਹੁੰਚ ਕਰਨ ਲਈ ਲਿੰਕ ਨੂੰ ਦੀ ਪਾਲਣਾ ਕਰੋ.';
+$lang['print_server_management_tool_not_accessible'] = 'ਜਦ ਸਰਵਰ ਨਾ ਚੱਲ ਰਿਹਾ ਹੈ ਪ੍ਰਿੰਟ ਸਰਵਰ ਪਰਬੰਧਨ ਸੰਦ ਉਪਲੱਬਧ ਨਹੀ ਹੈ';
