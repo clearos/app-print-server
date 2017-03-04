97ccd7a89d936303adfe8e68d556d0de7e1ec4b2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:09:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'يوفر تطبيق ملقم الطباعة المتقدمة واجهة الطباعة الشائعة إلى الطابعات على الشبكة. هذا يسمح للعملاء على الشبكة المحلية لتبادل الموارد المطبوعة ويخفف الإدارة في خدمة مركزية واحدة.';
+$lang['print_server_app_name'] = 'ملقم الطباعة المتقدمة';
+$lang['print_server_go_to_management_tool'] = 'الذهاب إلى طباعة أداة إدارة الملقم';
+$lang['print_server_management_tool'] = 'طباعة أداة إدارة الملقم';
+$lang['print_server_management_tool_help'] = 'اتبع الرابط للوصول إلى أداة إدارة ملقم الطباعة.';
+$lang['print_server_management_tool_not_accessible'] = 'أداة إدارة ملقم الطباعة غير متوفرة عندما لا يتم تشغيل الملقم';
