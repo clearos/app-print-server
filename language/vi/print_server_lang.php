45c53ca0e423b2678e11cc65c7431a873fc37ec0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:06:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Các ứng dụng Print Server Advanced cung cấp một giao diện in chung cho các máy in trên mạng. Điều này cho phép các khách hàng trên mạng LAN để chia sẻ tài nguyên in ấn và làm dịu chính quyền thành một trong những dịch vụ tập trung.';
+$lang['print_server_app_name'] = 'Nâng cao Print Server';
+$lang['print_server_go_to_management_tool'] = 'Tới In Công cụ quản lý máy chủ';
+$lang['print_server_management_tool'] = 'In Công cụ quản lý máy chủ';
+$lang['print_server_management_tool_help'] = 'Thực hiện theo các liên kết để truy cập vào các công cụ quản lý Print Server.';
+$lang['print_server_management_tool_not_accessible'] = 'Các công cụ quản lý Print Server không có sẵn khi máy chủ không chạy';
