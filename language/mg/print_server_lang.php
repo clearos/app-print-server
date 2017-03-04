61b733b5b0f760d81452451456960b29ac261fba
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:41:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Ny Advanced Print Server rindrambaiko dia manome iraisana ny mpanonta pirinty interface tsara eo amin&#39;ny tambajotra. Izany dia mamela mpanjifa eo amin&#39;ny LAN mba hizara pirinty loharanon-karena sy ny fitantanana ho iray eases foibe fanompoana.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Mandehana any amin&#39;ny Print Server Management Fitaovana';
+$lang['print_server_management_tool'] = 'Print Server Management Fitaovana';
+$lang['print_server_management_tool_help'] = 'Araho ny rohy mba mahazo ny Print Server fitantanana ny fitaovana.';
+$lang['print_server_management_tool_not_accessible'] = 'The Print Server fitaovana fitantanana dia tsy misy raha tsy ny mpanolotra mihazakazaka';
