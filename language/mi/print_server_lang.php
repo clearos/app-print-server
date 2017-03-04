8ca811f3614e7c3100e5e132e8fdc94c24e57d3f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:42:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Te taupānga Tūmau Tā Arā whakarato he atanga tā noa ki ngā pūreretā i runga i te whatunga. Tenei taea kiritaki i runga i te LAN ki te faaite i ngā rauemi tā me Kirimina whakahaere ki tetahi mahi centralized.';
+$lang['print_server_app_name'] = 'Tūmau Tā Arā';
+$lang['print_server_go_to_management_tool'] = 'Haere ki te Tāngia Tool Whakahaere Tūmau';
+$lang['print_server_management_tool'] = 'Tāngia Tool Whakahaere Tūmau';
+$lang['print_server_management_tool_help'] = 'A pee i te hono ki te uru i te taputapu whakahaere Tūmau Tā.';
+$lang['print_server_management_tool_not_accessible'] = 'E kore te mea e wātea ana te taputapu whakahaere Tūmau Tā, no te kore te rere i te tūmau';
