949e558b55ea7e131a857df8428558a6800ddda9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 20:46:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/print_server_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['print_server_app_description'] = 'Aplikasi Print Server Advanced menyediakan antara muka cetak biasa untuk pencetak pada rangkaian. Ini membolehkan pelanggan pada LAN untuk berkongsi sumber cetak dan memudahkan pentadbiran ke dalam satu perkhidmatan berpusat.';
+$lang['print_server_app_name'] = 'Advanced Print Server';
+$lang['print_server_go_to_management_tool'] = 'Pergi ke Cetak Alat Pengurusan Server';
+$lang['print_server_management_tool'] = 'Cetak Alat Pengurusan Server';
+$lang['print_server_management_tool_help'] = 'Ikut pautan untuk mengakses alat Cetak pengurusan Server.';
+$lang['print_server_management_tool_not_accessible'] = 'Alat pengurusan Print Server tidak boleh didapati apabila server tidak berjalan';
